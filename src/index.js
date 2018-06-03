2b5729a88ebeff98c69691d5bfafb49d39f60fc0
123surya <suryakumar.surendran@gmail.com>
2018-06-03 17:46:28 +0530

    added video detail

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React,{Component} from "react";
 import ReactDOM from "react-dom";
 import SearchBar from "./components/search-bar";
+import VideoList from "./components/video_list";
+import YTSearch from "youtube-api-search";
 
 const API_KEY = 'AIzaSyBKEPLX2cGNClLrM_PGwgn6490JXJZmIgM';
 
-const App = function(){
-    return (<div>
-                <SearchBar />
-            </div>);
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {videos:[] };
+        YTSearch({"key":API_KEY , term:'surfboards'},(videos)=> this.setState({videos}));
+    }
+ 
+    render(){
+        return (<div>
+                    <SearchBar />
+                    <VideoList videos = {this.state.videos} />
+                 </div>);
+    }
 };
 
 ReactDOM.render(<App/>,document.querySelector('.container'));
